27b1e758a18c284aa235089f4297bcc6a3938e4b
limingxian <305216313@qq.com>
2021-01-15 16:34:09 +0800

    线上商城demo

---
+++ src/components/Left.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <div class="title">热门推荐</div>
+        <ul class="menu" v-for="menuInfo in menuList" v-bind:key="menuInfo.id">
+            <li @click="menuInfo.click">
+				<p >{{menuInfo.name}}</p>
+			</li>
+        </ul>
+    </div>
+</template>
+<script>
+import Msg from './msg.js'
+export default {
+	data() {
+		return {
+			menuList: [{
+				id: 1,
+				name: '笔记本电脑',
+				click: function () {
+					Msg.$emit('val', '1')
+				}
+			}, {
+				id: 2,
+				name: '手机',
+				click: function () {
+					Msg.$emit('val', '2')
+				}
+			}, {
+				id: 3,
+				name: '服饰',
+				click: function () {
+					Msg.$emit('val', '3')
+				}
+			}, {
+				id: 4,
+				name: '手表',
+				click: function () {
+					Msg.$emit('val', '4')
+				}
+			}, {
+				name: '书籍',
+				click: function () {
+					Msg.$emit('val', '5')
+				}
+			}, {
+				name: '玩具',
+				click: function () {
+					Msg.$emit('val', '6')
+				}
+			}, {
+				name: '家电',
+				click: function () {
+					Msg.$emit('val', '7')
+				}
+			}, {
+				name: '学习用品',
+				click: function () {
+					Msg.$emit('val', '8')
+				}
+			}, {
+				name: '办公用品',
+				click: function () {
+					Msg.$emit('val', '9')
+				}
+			}, {
+				name: '旅游必备',
+				click: function () {
+					Msg.$emit('val', '10')
+				}
+			}, {
+				name: '游戏专栏',
+				click: function () {
+					Msg.$emit('val', '11')
+				}
+			}]
+		}
+	},
+	mounted: function() {
+		var _this=this;
+		Msg.$on('val', function(m) {
+			for (var i = 0; i < document.getElementsByClassName('menu').length; i++) {
+				document.getElementsByClassName('menu')[i]['style']['color'] = 'black';
+			}
+			document.getElementsByClassName('menu')[m-1]['style']['color'] = 'blue';
+		})
+	}
+}
+</script>
+<style type='text/css'>
+    .title{
+		width: 100px;
+		color: red;
+	}
+    .menu li{
+		list-style: none;						/* 去掉li前面的点 */
+		height: 50px;
+		margin-top: 2px;
+		background-color: white;
+		line-height: 50px;					/* 行高与height一致可居中 */
+		cursor: pointer;						/* 鼠标指针-手型 */
+	}
+	.menu li:active{
+		color: blue;
+	}
+	.menu li:default{
+		color: lightgrey;
+	}
+</style>
---
+++ src/components/Right.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div v-if="kk==='1'">
+            <GoodList :goodId='1'></GoodList>
+        </div>
+        <div v-else-if="kk==='2'">
+            <GoodList :goodId='2'></GoodList>
+        </div>
+        <div v-else-if="kk==='3'">
+            <GoodList></GoodList>
+        </div>
+        <div v-else>
+            <GoodList :goodId='0'></GoodList>
+        </div>
+    </div>
+</template>
+<script>
+    import Msg from './msg.js'
+    import GoodList from './GoodList'
+    export default {
+        components: {
+            GoodList
+        },
+        data() {
+            return {
+                kk: 0
+            }
+        },
+        mounted: function() {
+            var _this=this;
+            Msg.$on('val', function(m) {
+                _this.kk = m;
+            })
+        }
+    }
+</script>
